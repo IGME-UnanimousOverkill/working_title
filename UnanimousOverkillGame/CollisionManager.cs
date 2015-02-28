6b2e41ebed841ca73c3d6ef990b233c1444ee6a6
keegan Taylor <kmtaylor023@gmail.com>
2015-02-28 14:41:33 -0500

    I make collsion manager start yay

---
+++ UnanimousOverkillGame/CollisionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnanimousOverkillGame
+{
+    class CollisionManager
+    {
+        private List<GameObject> objects;
+        private List<PhysicsEntity> collisioCheckObjects;
+
+
+
+
+
+
+        public void CheckCollisions()
+        {
+            for (int col = 0; col < collisioCheckObjects.Count; col++)
+            {
+                for (int obj = 0; obj < objects.Count; obj++)
+                {
+                    if (objects.ElementAt(obj) != collisioCheckObjects.ElementAt(col) && collisioCheckObjects.ElementAt(col).Rect.Intersects(objects.ElementAt(obj).Rect))
+                    {
+                        //previous position and current position required? or do we not think clipping will happen
+                        //i do fix position here
+                        //i feel there needs to be a way for the movables to know they've collided, like an on collision method 
+
+                    }
+                }
+            }
+        }
+
+        public CollisionManager(GameObject[] allGameObjects, params PhysicsEntity[] objectsToBeChecked)
+        {
+            objects = new List<GameObject>(allGameObjects);
+            collisioCheckObjects = new List<PhysicsEntity>(objectsToBeChecked);
+        }
+
+    }
+}
