ebed536541adcd69dc9213a12ea0ee4e4259075a
Sean Coffey <sean@coffeyhouse.com>
2015-03-18 15:20:12 -0400

    Maptool can change gridsize

--- UnanimousOverkillMapTool/Map Tool/Form1.Designer.cs
+++ UnanimousOverkillMapTool/Map Tool/Form1.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.widthNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.WidthLabel = new System.Windows.Forms.Label();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.heightNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.widthNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(199, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(591, 506);
+            this.panel1.TabIndex = 0;
+            // 
+            // widthNumericUpDown
+            // 
+            this.widthNumericUpDown.Location = new System.Drawing.Point(63, 10);
+            this.widthNumericUpDown.Name = "widthNumericUpDown";
+            this.widthNumericUpDown.Size = new System.Drawing.Size(70, 22);
+            this.widthNumericUpDown.TabIndex = 1;
+            this.widthNumericUpDown.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.widthNumericUpDown.ValueChanged += new System.EventHandler(this.widthNumericUpDown_ValueChanged);
+            // 
+            // WidthLabel
+            // 
+            this.WidthLabel.AutoSize = true;
+            this.WidthLabel.Location = new System.Drawing.Point(13, 12);
+            this.WidthLabel.Name = "WidthLabel";
+            this.WidthLabel.Size = new System.Drawing.Size(44, 17);
+            this.WidthLabel.TabIndex = 3;
+            this.WidthLabel.Text = "Width";
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Location = new System.Drawing.Point(13, 48);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(49, 17);
+            this.HeightLabel.TabIndex = 5;
+            this.HeightLabel.Text = "Height";
+            // 
+            // heightNumericUpDown
+            // 
+            this.heightNumericUpDown.Location = new System.Drawing.Point(63, 46);
+            this.heightNumericUpDown.Name = "heightNumericUpDown";
+            this.heightNumericUpDown.Size = new System.Drawing.Size(70, 22);
+            this.heightNumericUpDown.TabIndex = 4;
+            this.heightNumericUpDown.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.heightNumericUpDown.ValueChanged += new System.EventHandler(this.heightNumericUpDown_ValueChanged);
             // 
             // MapTool
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 530);
+            this.Controls.Add(this.HeightLabel);
+            this.Controls.Add(this.heightNumericUpDown);
+            this.Controls.Add(this.WidthLabel);
+            this.Controls.Add(this.widthNumericUpDown);
+            this.Controls.Add(this.panel1);
             this.Name = "MapTool";
             this.Text = "MapTool";
+            ((System.ComponentModel.ISupportInitialize)(this.widthNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown widthNumericUpDown;
+        private System.Windows.Forms.Label WidthLabel;
+        private System.Windows.Forms.Label HeightLabel;
+        private System.Windows.Forms.NumericUpDown heightNumericUpDown;
+
     }
 }
 
--- UnanimousOverkillMapTool/Map Tool/Form1.cs
+++ UnanimousOverkillMapTool/Map Tool/Form1.cs
@@ -12,9 +12,48 @@ namespace Map_Tool
 {
     public partial class MapTool : Form
     {
+        private List<PictureBox> tiles;
+
+        private const int TILE_SIZE = 20;
+
         public MapTool()
         {
             InitializeComponent();
+            tiles = new List<PictureBox>(400);
+            populateTiles();
+        }
+
+        private void populateTiles()
+        {
+            foreach (PictureBox box in tiles)
+            {
+                box.Dispose();
+            }
+            tiles.Clear();
+            for (int y = 0; y < heightNumericUpDown.Value; y++)
+            {
+                for (int x = 0; x < widthNumericUpDown.Value; x++)
+                {
+                    PictureBox tile = new PictureBox();
+                    tile.BringToFront();
+                    tile.Parent = panel1;
+                    tile.Location = new Point(x * TILE_SIZE, y * TILE_SIZE);
+                    tile.Size = new Size(TILE_SIZE, TILE_SIZE);
+                    tile.Image = Image.FromFile("Content/placeholder.png");
+                    tiles.Add(tile);
+                    panel1.Controls.Add(tile);
+                }
+            }
+        }
+
+        private void widthNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            populateTiles();
+        }
+
+        private void heightNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            populateTiles();
         }
     }
 }
